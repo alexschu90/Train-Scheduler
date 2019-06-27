b9eafb183ef48e3b60b7d18335218cde7d49544c
Alex Chu <chu.alex9102@gmail.com>
2019-06-27 18:32:25 -0400

    updated script page to show all added inputs to show up on realtime database and html respectively. Must work on the time conversions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,5 +7,58 @@ var firebaseConfig = {
     messagingSenderId: "108557626599",
     appId: "1:108557626599:web:767162b4c7ece3ea"
   };
-  
+
   firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database(); 
+
+  $("#add-train").on("click", function(event) {
+      event.preventDefault(); 
+
+      var nTrainName = $("#train-name-input").val().trim();
+      var nDestination = $("#destination-input").val().trim();
+      // nFirstTrain = first train time need computation
+      var nFrequency = $("#frequency-input").val().trim();
+
+      var trainInfo = {
+          name: nTrainName,
+          destination: nDestination,
+          //first: nFirstTrain,
+          frequency: nFrequency
+      };
+
+      database.ref().push(trainInfo);
+
+      console.log(trainInfo.name);
+      console.log(trainInfo.destination);
+      //console.log(trainInfo.first);
+      console.log(trainInfo.frequency);
+
+      $("#train-name-input").val("");
+      $("#destination-input").val("");
+      //("#first-train-time-input").val("");
+      $("#frequency-input").val("");
+  })
+
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var nTrainName = childSnapshot.val().name;
+    var nDestination = childSnapshot.val().destination;
+    // var nFirstTrain = first train time need computation
+    var nFrequency = childSnapshot.val().frequency;
+
+    console.log(nTrainName);
+    console.log(nDestination);
+    //console.log(nFirstTrain);
+    console.log(nFrequency);
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(nTrainName),
+        $("<td>").text(nDestination),
+        // $("<td>").text(nFirstTrain),
+        $("<td>").text(nFrequency)
+    );
+
+    $("#train-schedule > tbody").append(newRow);
+  })
