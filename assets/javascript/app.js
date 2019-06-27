b9e14a7870dda58f4bafacdc4e2b934f8c62400b
Alex Chu <chu.alex9102@gmail.com>
2019-06-27 17:56:15 -0400

    set up config for firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA2nLb-YmfL0E78fjqx4aC2Xupgi0-_UaI",
+    authDomain: "train-scheduler-a3dda.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a3dda.firebaseio.com",
+    projectId: "train-scheduler-a3dda",
+    storageBucket: "",
+    messagingSenderId: "108557626599",
+    appId: "1:108557626599:web:767162b4c7ece3ea"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
